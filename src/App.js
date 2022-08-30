40e5c2607b5d4500af0823a99b1a242821dfbbef
Giorgia <jofoxter@gmail.com>
2022-08-30 18:20:25 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,28 +5,29 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      {" "}
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://polite-cassata-f5e2ec.netlify.app/
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://polite-cassata-f5e2ec.netlify.app/
         "
-          target="_blank"
-          rel="noreferrer"
-        >
-          Giorgia Francesca Costantino{" "}
-        </a>
-        and is {""}
-        <a
-          href="https://github.com/Jofoxter/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced on GitHub
-        </a>
-      </footer>
+            target="_blank"
+            rel="noreferrer"
+          >
+            Giorgia Francesca Costantino{" "}
+          </a>
+          and is {""}
+          <a
+            href="https://github.com/Jofoxter/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 
