6f2ce533b89690a6e0d3bf941b76a36f4efa4f30
Giorgia <jofoxter@gmail.com>
2022-08-30 22:05:07 +0200

    Implemented basic CSS

--- src/weather.js
+++ src/weather.js
@@ -4,31 +4,38 @@ import "./weather.css";
 export default function Weather () {
 return (
   <div className="weather">
-        <form>
-    <div className="row">
-            <div className="col-9">
+    <form>
+      <div className="row">
+        <div className="col-9">
           <input
             type="search"
-            placeholder="Eneter a city..."
+            placeholder="Enter a city..."
             className="form-control"
-          /></div>
-          <div className="col-3">
-        <input type="submit" value="Search" className="btn btn-primary" />
-          </div>
-             </div>
-      </form>
+            autoFocus="on"
+          />
+        </div>
+        <div className="col-3">
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-primary w-100"
+          />
+        </div>
+      </div>
+    </form>
     <h1>Lugano</h1>
     <ul>
       <li>Wednesday 07:00</li>
       <li>Mostly Cloudy</li>
     </ul>
-    <div className="row">
+    <div className="row mt-3">
       <div className="col-6">
-        <img
-          src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-          alt="Mostly Cloudy"
-        />
-        6°C
+        <div className="clearfix">
+          <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Mostly Cloudy" className="float-left"/>
+            <span className="temperature"> 6</span>
+            <span className="unit">°C</span>
+        </div>
       </div>
       <div className="col-6">
         <ul>
