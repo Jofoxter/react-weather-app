9854cc5a60e8b38ea88ba61db873b731d9341812
Giorgia <jofoxter@gmail.com>
2022-08-30 22:54:05 +0200

    Added API integration

--- src/weather.js
+++ src/weather.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./weather.css";
 
-export default function Weather () {
+export default function Weather (props) {
+    let [ready,setReady] = useState(false);
+    let [weatherData, setWeatherData] = useState({ ready: false});
+ function handleResponse (response) {
+    setWeatherData ({
+        ready: true,
+        temperature: response.data.main.temp,
+        humidity: response.data.main.humidity,
+        description: response.data.weather[0].description,
+        date:"Wednesday 07:00",
+        iconUrl:"https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+        wind: response.data.wind.speed,
+        city: response. data.name
+    })
+ }
+
+if (weatherData.ready) {
 return (
   <div className="weather">
     <form>
@@ -23,28 +40,44 @@ return (
         </div>
       </div>
     </form>
-    <h1>Lugano</h1>
+    <h1>{weatherData.city}</h1>
     <ul>
-      <li>Wednesday 07:00</li>
-      <li>Mostly Cloudy</li>
+      <li>{weatherData.date}</li>
+      <li className="text-capitalize">{weatherData.description}</li>
     </ul>
     <div className="row mt-3">
       <div className="col-6">
         <div className="clearfix">
-          <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly Cloudy" className="float-left"/>
-            <span className="temperature"> 6</span>
-            <span className="unit">°C</span>
+          <img
+            src={weatherData.iconUrl}
+            alt={weatherData.description}
+            className="float-left"
+          />
+          <span className="temperature">
+            {" "}
+            {Math.round(weatherData.temperature)}
+          </span>
+          <span className="unit">°C</span>
         </div>
       </div>
       <div className="col-6">
         <ul>
-          <li>Precipitation: 15%</li>
-          <li>Humidity: 72%</li>
-          <li>Wind: 13 km/h</li>
+          <li>Humidity: {weatherData.humidity}%</li>
+          <li>Wind: {weatherData.wind} km/h</li>
         </ul>
       </div>
     </div>
   </div>
 );
+} else {
+   let apiKey = "18587a413f83472ff5f95d93ae688338";
+   let city = "Lugano";
+   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+   axios.get(apiUrl).then(handleResponse); 
+
+   return "Loading...";
+}
+
+
+
 }
