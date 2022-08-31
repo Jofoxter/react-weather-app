6525fb620341fa3af4439cdf6cabe1260d3a8e2c
Giorgia <jofoxter@gmail.com>
2022-08-31 19:11:28 +0200

    °F&°C setup

--- src/Weatherinfo.js
+++ src/Weatherinfo.js
@@ -1,6 +1,8 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
+
 
 export default function WeatherInfo (props) {
 return (
@@ -14,16 +16,16 @@ return (
     </ul>
     <div className="row mt-3">
       <div className="col-6">
-        <div className="clearfix">
-        <WeatherIcon code={props.data.icon} />
-        <span className="temperature">
-              {" "}
-              {Math.round(props.data.temperature)}
-            </span>
-            <span className="unit">°C</span>
+        <div className="d-flex">
+          <div>
+            <WeatherIcon code={props.data.icon} />
+          </div>
+          <div>
+            <WeatherTemperature celsius={props.data.temperature} />
           </div>
         </div>
-         <div className="col-6">
+      </div>
+      <div className="col-6">
         <ul>
           <li>Humidity: {props.data.humidity}%</li>
           <li>Wind: {props.data.wind} km/h</li>
